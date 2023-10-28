10a77cf6a6a20b55c397bbe4c3df53261b3cd0c2
SAI PRADEEP VAMALA <SAI PRADEEP VAMALA@PRADEEP>
2023-10-28 11:02:39 +0530

    Added logic with Explicit wait

--- SelIntro/src/ecommerceApp/Base.java
+++ SelIntro/src/ecommerceApp/Base.java
@@ -3,26 +3,32 @@ package ecommerceApp;
 import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Base {
 
 	public static void main(String[] args) {
+		// Explicit wait can be acheived in two ways:
+		// 1: WebDriver wait
+		// 2: Explicit Fluent
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://rahulshettyacademy.com/seleniumPractise/#/");
 		// Implicit Wait of 5 seconds
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		// driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
 		driver.manage().window().maximize();
 		addProductToCart(driver);
 		driver.findElement(By.cssSelector("img[alt='Cart']")).click();
 		driver.findElement(By.xpath("//button[text()='PROCEED TO CHECKOUT']")).click();
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[contains(@placeholder,'promo')]")));
 		driver.findElement(By.xpath("//*[contains(@placeholder,'promo')]")).sendKeys("rahulshettyacademy");
 		driver.findElement(By.cssSelector(".promoBtn")).click();
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("promoInfo")));
 		System.out.println(driver.findElement(By.className("promoInfo")).getText());
 	}
 
